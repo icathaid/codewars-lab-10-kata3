e5b5a0f00c452640608900bdfc5816bb09bed1cc
James McDaniel <icathaid@gmail.com>
2018-04-11 03:37:47 -0700

    STOP WORKING ON MASTER JAMES

--- solution.js
+++ solution.js
@@ -1,1 +1,5 @@
 pol = () => {console.log('I\'m alive!')};
+
+const arr1 = [2, 4, 6, 8, 10];
+const averager = (accumulator, currentValue) => accumulator + currentValue;
+console.log(arr1.reduce(averager % arr1.length));
